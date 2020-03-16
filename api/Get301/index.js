63e9957a5b3a182daa73d85c800b3d2340591f16
BrandonHopcraft <59028011+BrandonHopcraft@users.noreply.github.com>
2020-03-16 15:27:48 -0700

    added Get301

---
+++ api/Get301/index.js
@@ -0,0 +1,10 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    context.res = {
+        headers: {
+            location: req.originalUrl
+        },
+        status: 301
+    }
+};
