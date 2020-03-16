75ca4819f89f6036b85346a92d91e7b5c9d02c4c
BrandonHopcraft <59028011+BrandonHopcraft@users.noreply.github.com>
2020-03-16 14:10:47 -0700

    Update index.js

--- api/GetList/index.js
+++ api/GetList/index.js
@@ -2,6 +2,6 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.res = {
-        body: ["Jarod", "Hannah", "Kevin", "Mitch", "chris", "Joe", "Mitali", "Annaji", 1, 2, 3]
+        body: ["Jarod", "Hannah", "Kevin", "Mitch", "chris", "Joe", "Mitali", "Annaji", 1, 2, 3, 4]
     }
 };
